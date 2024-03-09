e762df473fd67b78cbaa44601972d5f64f225f08
Duc Huy Nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2024-03-09 11:05:05 +0100

    Obstacles objects added

---
+++ src/model/ModelObstacle.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.awt.*;
+import java.util.Random;
+
+public class ModelObstacle extends ModelUnit {
+    private String imgLink;
+
+    public ModelObstacle(int id, Point position) {
+        super(id, position, position);
+
+        Random rand = new Random();
+        int n = rand.nextInt(4) + 1;
+        imgLink = "src/assets/maingame/obstacle/" + n + ".png";
+    }
+
+    public String getImgLink() {
+        return this.imgLink;
+    }
+}
